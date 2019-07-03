13d8a5320e50068c7ac593f7034fed675370014b
AJ <alvianjoseph@gmail.com>
2019-07-02 20:01:15 -0700

    all tests passing

--- __tests__/lib/reader-async.test.js
+++ __tests__/lib/reader-async.test.js
@@ -2,12 +2,12 @@
 
 jest.mock('fs');
 
-const reader = require('../../lib/reader.js');
+const reader = require('../../lib/reader-async.js');
 
 describe('File Reader Module', () => {
 
   it('when given a bad file, returns an error', done => {
-    let files = ['bad.txt'];
+    let files = ['file1.txt', 'file2.txt', 'file2.txt'];
     // In jest, throwing errors obviously kills the app, so if you're
     // going to throw one in a test, have the expect execute your code as a
     // function so that you can trap it.
--- edit-file.js
+++ edit-file.js
@@ -6,18 +6,18 @@ const faker = require('faker');
 let file = process.argv[2];
 
 let myFunction = (err, data) => {
-    if (err) { throw err; }
-    console.log(`Before:`, data.toString());
+  if (err) { throw err; }
+  console.log(`Before:`, data.toString());
     
-    let randomNum = faker.random.number();
-    fs.appendFile(file, randomNum, (err) => {
-        if (err) { throw err; }
-        console.log(`${randomNum} was added to ${file}`);
-    })
-    fs.readFile(file, (err, data) => {
-        if (err) { throw err; }
-        console.log(`After:`, data.toString());
-    })
+  let randomNum = faker.random.number();
+  fs.appendFile(file, randomNum, (err) => {
+    if (err) { throw err; }
+    console.log(`${randomNum} was added to ${file}`);
+  })
+  fs.readFile(file, (err, data) => {
+    if (err) { throw err; }
+    console.log(`After:`, data.toString());
+  })
 }
 
 //initial console log is undefined if I don't read here
